0fab62bc065757243b34956060df0a4ee7b80b98
Christian <artychristian@gmail.com>
2019-11-22 02:24:17 -0500

    Dang I must've been bored...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import theBosMachine from "./assets/images/baus.jpeg";
+import elToroLoco from "./assets/images/towhisky.jpg";
+import tweettweet from "./assets/images/tweettweet.svg";
+import theSource from './assets/images/github.png'
+const App = () => {
+  const [isflipped, setFlipped] = useState(false);
 
-function App() {
+  const flipThatSucker = () => {
+    setFlipped(!isflipped);
+  };
+
+  const determineLink = () => {
+    const link = document.getElementById("twitterFingers");
+    const twitter = "https://twitter.com";
+    if (isflipped) {
+      link.href = `${twitter}/stolinski`;
+    } else {
+      link.href = `${twitter}/wesbos`;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id="upsideDown" className={`App ${isflipped ? "onHead" : ""} `}>
+      <header className={`App-header `}>
+        <h1 style={{}}>Stand on your head</h1>
+        <p>Best Problem Solving Tool Out There</p>
+        <div className="faces">
+          <img
+            src={isflipped ? elToroLoco : theBosMachine}
+            alt="Either a person Wes Bos or of Scott Tolinski"
+          ></img>{" "}
+        </div>
+
+        <div
+          className='twitter'
         >
-          Learn React
-        </a>
+          <a id="twitterFingers" href="#!">
+            <img
+              src={tweettweet}
+              alt="A twitter icon linking to respective twitters"
+              onClick={determineLink}
+              style={{border:'0.5px solid white', borderRadius:'5px'}}
+            ></img>
+          </a>
+        </div>
+        <button
+          className={`flipper ${isflipped ? "onHead" : ""}`}
+          onClick={flipThatSucker}
+        >
+          {isflipped ? "You flipped it. You proud?" : "Totally doesn't flip the face"}
+        </button>
       </header>
+      <footer>
+      <div><a href='https://www.github.com/christianarty'><img src={theSource} alt='Link to the source repo'></img></a></div>
+      by Christian Arty
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
