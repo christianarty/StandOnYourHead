b3b5d89b50d98e87fb130ff2a15d7132df24ba90
Christian Arty <artychristian@gmail.com>
2020-09-04 18:57:43 -0400

    kenny button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.scss";
 import theBosMachine from "./assets/images/baus.jpeg";
 import elToroLoco from "./assets/images/towhisky.jpg";
 import tweettweet from "./assets/images/tweettweet.svg";
-import theSource from './assets/images/github.png'
+import theSource from "./assets/images/github.png";
 const App = () => {
   const [isFlipped, setFlipped] = useState(false);
 
@@ -25,7 +25,7 @@ const App = () => {
     <div id="upsideDown" className={`App ${isFlipped ? "onHead" : ""} `}>
       <header className={`main `}>
         <h1>Stand on your head</h1>
-        <p>The Best Problem Solving Tool Out There</p>
+        <p>The Best Kenny Solving Tool Out There</p>
         <div className="faces">
           <img
             src={isFlipped ? elToroLoco : theBosMachine}
@@ -33,15 +33,13 @@ const App = () => {
           ></img>
         </div>
 
-        <div
-          className='twitter'
-        >
+        <div className="twitter">
           <a id="twitterFingers" href="#!">
             <img
               src={tweettweet}
               alt="A twitter icon linking to respective twitters"
               onClick={determineLink}
-              style={{border:'0.5px solid white', borderRadius:'5px'}}
+              style={{ border: "0.5px solid white", borderRadius: "5px" }}
             ></img>
           </a>
         </div>
@@ -49,12 +47,24 @@ const App = () => {
           className={`flipper ${isFlipped ? "onHead" : ""}`}
           onClick={flipThatSucker}
         >
-          {isFlipped ? "You flipped it. You proud?" : "Totally doesn't flip the face"}
+          {isFlipped
+            ? "You flipped it. You proud?"
+            : "Totally doesn't flip the face"}
+        </button>
+        <button
+          className={`flipper ${isFlipped ? "onHead" : ""}`}
+          onClick={flipThatSucker}
+        >
+          Kenny Button
         </button>
       </header>
       <footer>
-      <div><a href='https://www.github.com/christianarty/StandOnYourHead'><img src={theSource} alt='Link to the source repo'></img></a></div>
-      by Christian Arty
+        <div>
+          <a href="https://www.github.com/christianarty/StandOnYourHead">
+            <img src={theSource} alt="Link to the source repo"></img>
+          </a>
+        </div>
+        by Christian Arty
       </footer>
     </div>
   );
